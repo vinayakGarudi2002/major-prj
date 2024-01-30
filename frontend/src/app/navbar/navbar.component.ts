b20a08f5ddf5898dc66f72591b0a5193f2f139e7
vinayak <vinayakgarudi45@gmail.com>
2024-01-30 17:09:32 +0530

    routing done

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -10,8 +10,16 @@ export class NavbarComponent {
   status: boolean = true;
 
   name: string|null = localStorage.getItem('NAME');
+  addr: string|null = localStorage.getItem('WALLETID');
+  showActiveTenders: boolean;
+  showTenders: boolean;
+  constructor() {
+    // Use constructor for initialization
+    this.showActiveTenders = !(this.addr === "0xa3df8bd4bd75f724ef51e50f3309dd5943fd648e");
+    this.showTenders=(this.addr === "0xa3df8bd4bd75f724ef51e50f3309dd5943fd648e");
+  }
 
-  toggleButton(){
-      this.status = !this.status;       
+  toggleButton() {
+    this.status = !this.status;
   }
 }
